ff5179ac12839e8b832f8445fdf5d470a7cf4bb1
Bakanych <manych@gmail.com>
2022-02-12 23:30:25 +0800

    game

--- index.js
+++ index.js
@@ -6,17 +6,61 @@ function tree(a, b, depth, target) {
     [a, b]
         .sort(() => Math.random() - 0.5)
         .forEach((x) => {
+        let link = document.createElement("a");
+        link.innerHTML = `<mark>${x}</mark>`;
         let li = document.createElement("li");
         let ul = document.createElement("ul");
-        li.innerHTML = x;
+        //   li.innerHTML = x;
         target.appendChild(li);
+        li.appendChild(link);
         li.appendChild(ul);
         tree(a, b, depth - 1, ul);
     });
     return target;
 }
+// get node children
+// get all nodes from tree paths from root to leaf and put them to arrays
+function getAllNodes(node) {
+    let nodes = [];
+    if (node.childNodes.length == 0) {
+        nodes.push(node);
+        return nodes;
+    }
+    node.childNodes.forEach((child) => {
+        nodes = nodes.concat(getAllNodes(child));
+    });
+    return nodes;
+}
+function getPath() {
+    function iterate(node, res) {
+        let children = node.querySelectorAll("ul>li");
+        if (children.length) {
+            return children.forEach((x) => iterate(x, res.concat(x)));
+        }
+        if (res.length >= 4)
+            result.push(res);
+    }
+    const target = document.getElementById("tree");
+    let result = [];
+    iterate(target, []);
+    const pathElement = document.getElementById("path");
+    const randomPath = result
+        .sort(() => Math.random() - 0.5)[0]
+        .map((x) => {
+        const mark = x.querySelector("mark");
+        //   pathElement.appendChild(mark);
+        return mark;
+    });
+    pathElement.innerHTML += randomPath.map((x) => x === null || x === void 0 ? void 0 : x.outerHTML).join(".");
+    // randomPath.forEach((x) => x!.classList.add("secondary"));
+    return randomPath;
+}
 function startTimer(target) {
+    var _a, _b;
     target.setAttribute("disabled", "true");
+    const path = getPath();
+    (_b = (_a = document
+        .getElementById("path")) === null || _a === void 0 ? void 0 : _a.querySelectorAll("mark")) === null || _b === void 0 ? void 0 : _b.forEach((x, i) => (x.innerText = path[i].innerText));
     let start = Date.now();
     let interval = setInterval(() => {
         let now = Date.now();
--- index.ts
+++ index.ts
@@ -10,18 +10,67 @@ function tree(
   [a, b]
     .sort(() => Math.random() - 0.5)
     .forEach((x) => {
+      let link = document.createElement("a");
+      link.innerHTML = `<mark>${x}</mark>`;
       let li = document.createElement("li");
       let ul = document.createElement("ul");
-      li.innerHTML = x;
+      //   li.innerHTML = x;
       target.appendChild(li);
+      li.appendChild(link);
       li.appendChild(ul);
       tree(a, b, depth - 1, ul);
     });
   return target;
 }
 
+// get node children
+
+// get all nodes from tree paths from root to leaf and put them to arrays
+function getAllNodes(node: Node): Node[] {
+  let nodes: Node[] = [];
+  if (node.childNodes.length == 0) {
+    nodes.push(node);
+    return nodes;
+  }
+  node.childNodes.forEach((child) => {
+    nodes = nodes.concat(getAllNodes(child));
+  });
+  return nodes;
+}
+
+function getPath(): HTMLElement[] {
+  function iterate(node: Element, res: Element[]): void {
+    let children = node.querySelectorAll("ul>li");
+    if (children.length) {
+      return children.forEach((x) => iterate(x, res.concat(x)));
+    }
+    if (res.length >= 4) result.push(res);
+  }
+
+  const target = document.getElementById("tree")!;
+  let result: HTMLElement[][] = [];
+  iterate(target, []);
+  const pathElement = document.getElementById("path")!;
+  const randomPath = result
+    .sort(() => Math.random() - 0.5)[0]
+    .map((x) => {
+      const mark = x.querySelector("mark");
+      //   pathElement.appendChild(mark);
+      return mark;
+    });
+  pathElement.innerHTML += randomPath.map((x) => x?.outerHTML).join(".");
+  // randomPath.forEach((x) => x!.classList.add("secondary"));
+
+  return randomPath;
+}
+
 function startTimer(target: HTMLElement) {
   target.setAttribute("disabled", "true");
+  const path = getPath();
+  document
+    .getElementById("path")
+    ?.querySelectorAll("mark")
+    ?.forEach((x, i) => (x.innerText = path[i].innerText));
   let start = Date.now();
   let interval = setInterval(() => {
     let now = Date.now();
